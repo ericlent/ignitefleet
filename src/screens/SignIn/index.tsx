4e686ea138439df0ac64f3d1c0d18c5193367175
ericlent <ericquaresma@hotmail.com>
2024-08-29 13:44:47 -0300

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,42 @@
+import { Container, Title, Slogan } from "./styles";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
+import backgroundImg from "../../assets/background.png";
+import { Button } from "../../components/Button";
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from "@env";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+GoogleSignin.configure({
+  scopes: ["email", "profile"],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+
+})
+
+export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function hangleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true);
+      const { idToken } = await GoogleSignin.signIn();
+
+      if (idToken) {
+
+      }
+
+    } catch (error) {
+      setIsAuthenticating(false);
+      Alert.alert("Entrar", "Não foi possível conectar na sua conta Google");
+    }
+  }
+
+  return (
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+      <Button title="Entrar com o Google" isLoading={isAuthenticating} onPress={hangleGoogleSignIn} />
+    </Container>
+  );
+}
