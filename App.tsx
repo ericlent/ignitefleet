57ff6b7116eb537e75a9b72b02bbf4e874e4d728
ericlent <ericquaresma@hotmail.com>
2024-09-03 17:52:43 -0300

    TopMessage

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import { REALM_APP_ID } from "@env";
 import { Routes } from "./src/routes";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RealmProvider, syncConfig } from "./src/libs/realm";
+import { TopMessage } from './src/components/TopMessage';
+import { WifiSlash } from 'phosphor-react-native';
 
 export default function App() {
   const fontsLoaded = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -24,16 +26,24 @@ export default function App() {
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
         <SafeAreaProvider style={{ backgroundColor: theme.COLORS.GRAY_800 }}>
+
           <StatusBar
             barStyle="light-content"
             backgroundColor="transparent"
             translucent
           />
+
+          <TopMessage
+            title='Você está off-line'
+            icon={WifiSlash}
+          />
+
           <UserProvider fallback={SignIn}>
             <RealmProvider sync={syncConfig} fallback={Loading}>
               <Routes />
             </RealmProvider>
           </UserProvider>
+
         </SafeAreaProvider>
       </ThemeProvider>
     </AppProvider>
