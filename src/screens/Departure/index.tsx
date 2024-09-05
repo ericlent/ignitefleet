fbe6a3f2cf2bdb4c45b981b28d8a369a4966005c
ericlent <ericquaresma@hotmail.com>
2024-09-05 13:39:36 -0300

    Geolocalização

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,16 +1,21 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
 import { TextAreaInput } from '../../components/TextAreaInput';
-import { Container, Content } from './styles';
+import { Container, Content, Message, MessageContent } from './styles';
 import { TextInput, ScrollView, Alert } from 'react-native';
-import { licensePlateValidate } from '../../utils/licensePlateValidate';
 import { useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
 import { useUser } from '@realm/react';
 import { useNavigation } from '@react-navigation/native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { LocationAccuracy, LocationSubscription, useForegroundPermissions, watchPositionAsync } from 'expo-location';
+import { licensePlateValidate } from '../../utils/licensePlateValidate';
+import { getAddressLocation } from '../../utils/getAddressLocation';
+import { Loading } from '../../components/Loading';
+import { LocationInfo } from '../../components/LocationInfo';
+import { Car } from 'phosphor-react-native';
 
 //Alternativa para tratar o posicionamento do Keyboard
 //const keyboardAvoidingViewBehavior = Platform.OS === 'android' ? 'height' : 'position';
@@ -19,6 +24,10 @@ export function Departure() {
   const [description, setDescription] = useState('');
   const [licensePlate, setLicensePlate] = useState('');
   const [isRegistering, setIsResgistering] = useState(false);
+  const [currentAddress, setCurrentAddress] = useState<string | null>(null)
+  const [isLoadingLocation, setIsLoadingLocation] = useState(true);
+
+  const [locationForegroundPermission, requestLocationForegroundPermission] = useForegroundPermissions();
 
   const descriptionRef = useRef<TextInput>(null);
   const licensePlateRef = useRef<TextInput>(null);
@@ -60,6 +69,56 @@ export function Departure() {
     }
   }
 
+  useEffect(() => {
+    requestLocationForegroundPermission();
+  }, []);
+
+  useEffect(() => {
+    if (!locationForegroundPermission?.granted) {
+      return;
+    }
+
+    let subscription: LocationSubscription;
+
+    watchPositionAsync({
+      accuracy: LocationAccuracy.High,
+      timeInterval: 1000
+    }, (location) => {
+      getAddressLocation(location.coords)
+        .then(address => {
+          if (address) {
+            setCurrentAddress(address)
+          }
+        })
+        .finally(() => setIsLoadingLocation(false))
+    }).then(response => subscription = response);
+
+    return () => {
+      if (subscription) {
+        subscription.remove()
+      }
+    };
+  }, [locationForegroundPermission?.granted])
+
+  if (!locationForegroundPermission?.granted) {
+    return (
+      <Container>
+        <Header title='Saída' />
+        <MessageContent>
+          <Message>
+            Você precisa permitir que o aplicativo tenha acesso a
+            localização para acessar essa funcionalidade. Por favor, acesse as
+            configurações do seu dispositivo para conceder a permissão ao aplicativo.
+          </Message>
+        </MessageContent>
+      </Container>
+    )
+  }
+
+  if (isLoadingLocation) {
+    return <Loading />
+  }
+
   return (
     <Container>
       <Header title='Saída' />
@@ -69,6 +128,16 @@ export function Departure() {
       <KeyboardAwareScrollView extraHeight={100}>
         <ScrollView>
           <Content>
+
+            {
+              currentAddress &&
+              <LocationInfo
+                icon={Car}
+                label='Localização atual'
+                description={currentAddress}
+              />
+            }
+
             <LicensePlateInput
               ref={licensePlateRef}
               label='Placa do veículo'
