68dae96bb08c3db68a869cfc854ce44bc0774a84
ericlent <ericquaresma@hotmail.com>
2024-09-06 09:13:05 -0300

    Adicionado as permissões de localização

--- app.config.js
+++ app.config.js
@@ -21,6 +21,9 @@ module.exports =
       "bundleIdentifier": "com.equal.ignitefleet",
       "config": {
         "googleMapsApiKey": process.env.GOOGLE_MAPS_API_KEY
+      },
+      "infoPlist": {
+        "UIBackgroundModes": ["location"]
       }
     },
     "android": {
@@ -29,6 +32,11 @@ module.exports =
         "backgroundColor": "#202024"
       },
       "package": "com.equal.ignitefleet",
+      "permissions": [
+        "ACCESS_FINE_LOCATION",
+        "ACCESS_COARSE_LOCATION",
+        "ACCESS_BACKGROUND_LOCATION"
+      ],
       "googleMaps": {
         "apiKey": process.env.GOOGLE_MAPS_API_KEY
       }
